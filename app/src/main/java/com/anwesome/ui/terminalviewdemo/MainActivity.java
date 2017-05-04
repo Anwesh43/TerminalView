1962eb4b5284b60bc9b03ad343da948248f9187a
Anwesh43 <anweshthecool0@gmail.com>
2017-05-04 17:17:06 +0530

    Using Terminal View in app

--- app/src/main/java/com/anwesome/ui/terminalviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/terminalviewdemo/MainActivity.java
@@ -3,11 +3,14 @@ package com.anwesome.ui.terminalviewdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.terminalview.TerminalView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TerminalView terminalView = new TerminalView(this);
+        setContentView(terminalView);
     }
 }
