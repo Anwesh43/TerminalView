423a86e1d5e18026044293e6e0e409e31195f6d1
Anwesh43 <anweshthecool0@gmail.com>
2017-05-04 12:38:11 +0530

    Adding terminalview module

---
+++ terminalview/src/main/java/com/anwesome/ui/terminalview/TerminalView.java
@@ -0,0 +1,17 @@
+package com.anwesome.ui.terminalview;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 04/05/17.
+ */
+public class TerminalView extends View {
+    public TerminalView(Context context){
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+
+    }
+
+}
